5e321ed6009efc07db498650877c9f18412f6dea
TanLP2003 <phutanle38@gmail.com>
2024-04-19 16:37:59 +0700

    le_phu_tan update

---
+++ src/components/layouts/AdminLayout/index.jsx
@@ -0,0 +1,7 @@
+const AdminLayout = () => {
+    return (
+        <div>Admin layout</div>
+    )
+}
+
+export default AdminLayout;
---
+++ src/components/layouts/CustomerLayout/index.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+const CustomerLayout = () => {
+    return <div>
+        <p>CustomerLayout</p>
+        <Outlet />
+    </div>
+}
+
+export default CustomerLayout;
