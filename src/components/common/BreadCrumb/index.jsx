7dbd5fdcbad4c3e7497af1b74794238a3633dee4
thanhnguyen <thanhnguyent472003@gmail.com>
2024-04-23 17:42:10 +0700

    #1 thanh_nguyen create footer and breadcrumb

---
+++ src/components/common/BreadCrumb/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const BreadCrumb = (props) => {
+  return (
+    <div className="BreadCrumb mb-0 py-4">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <p className="text-center">
+              <Link to="/" className="text-dark">
+                Home &nbsp;
+              </Link>
+              / {props.title}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BreadCrumb;
--- src/components/layouts/CustomerLayout/index.jsx
+++ src/components/layouts/CustomerLayout/index.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../common/Header";
-
+import { Footer } from "../../common/Footer";
 
 const CustomerLayout = () => {
-    return (
+  return (
     <>
-        <Header />
-        <Outlet />
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-    )
-}
+  );
+};
 
 export default CustomerLayout;
