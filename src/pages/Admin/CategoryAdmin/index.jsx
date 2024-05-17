80c2d78de99c00a2f5ec9d95aaaa0b591c1b4286
TanLP2003 <phutanle372@gmail.com>
2024-05-17 22:30:54 +0700

    Update

--- src/pages/Admin/CategoryAdmin/index.jsx
+++ src/pages/Admin/CategoryAdmin/index.jsx
@@ -1,19 +1,11 @@
-import { useEffect, useState } from "react";
-import CreateCategory from "./CreateCategory";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCategory } from "../../../redux/apis/category-api";
 import './style.scss'
 import useFetchData from "../../../components/hooks/useFetchData";
 const CategoryAdmin = () => {
-    // const [isFetched, setIsFetched] = useState(false);
     const dispatch = useDispatch();
     const isFetched = useFetchData(() => [dispatch(getAllCategory())]);
     const categories = useSelector((state) => state.categories.categories);
-    // useEffect(() => {
-    //     dispatch(getAllCategory())
-    //         .then(() => setIsFetched(true))
-    //         .catch(err => console.log(err));
-    // }, []);
     const renderCategoryList = () => {
         return categories.map((item, index) => {
             return (
