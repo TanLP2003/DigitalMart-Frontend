a6073bcd03d89c03c912ca0b30be1acd67352b62
Tan Nguyen <tann3946@gmail.com>
2024-05-28 17:53:29 +0700

    update admin navbar

--- src/components/layouts/AdminLayout/Topbar/index.jsx
+++ src/components/layouts/AdminLayout/Topbar/index.jsx
@@ -5,19 +5,6 @@ const Topbar = () => {
     return (
         <div className="topbar">
             <div className='brand'>Digital Mart</div>
-            <div className='topbar-list'>
-                <Link to='/admin/profile'>
-                    <div className='topbar-list-profile'>
-                        <img className='avatar' src="https://res.cloudinary.com/dumhujhqd/image/upload/v1715091840/hello-world/default_avatar_cg2kax.jpg" alt="" />
-                        <div>
-                            <div>ADMIN</div>
-                            <div>phutanle372@gmail.com</div>
-                        </div>
-                    </div>
-                </Link>
-                
-                <GoBell size={30} className='topbar-list-bell'/>
-            </div>
         </div>
     )
 }
